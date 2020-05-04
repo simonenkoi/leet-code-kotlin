091cd9187f65e9a6e5dfe18f130a657de987d520
Ignat Simonenko <simonenkoignat@gmail.com>
2020-05-04 10:02:36 +0300

    Add is same tree solution

---
+++ src/main/kotlin/is_same_tree/Solution.kt
@@ -0,0 +1,12 @@
+package is_same_tree
+
+class Solution {
+    fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+        if (p == null && q == null) {
+            return true
+        }
+        return p?.`val` == q?.`val`
+                && isSameTree(p?.left, q?.left)
+                && isSameTree(p?.right, q?.right)
+    }
+}
---
+++ src/main/kotlin/is_same_tree/TreeNode.kt
@@ -0,0 +1,6 @@
+package is_same_tree
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
