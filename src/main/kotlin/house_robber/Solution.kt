62f02e4fab38e069b81c27f1830f565d56a4d8a7
Ignat Simonenko <simonenkoignat@gmail.com>
2020-05-08 14:04:10 +0300

    Add memoization to house robber solution

--- src/main/kotlin/house_robber/Solution.kt
+++ src/main/kotlin/house_robber/Solution.kt
@@ -3,13 +3,23 @@ package house_robber
 import kotlin.math.max
 
 class Solution {
+
+    private lateinit var memoization: IntArray
+
     fun rob(nums: IntArray): Int {
+        memoization = IntArray(nums.size) { -1 }
         return rob(nums, nums.lastIndex)
     }
 
     private fun rob(nums: IntArray, i: Int): Int {
-        return if (i >= 0) {
-            max(rob(nums, i - 2) + nums[i], rob(nums, i - 1))
-        } else 0
+        if (i < 0) {
+            return 0
+        }
+        if (memoization[i] >= 0) {
+            return memoization[i]
+        }
+        val result: Int = max(rob(nums, i - 2) + nums[i], rob(nums, i - 1))
+        memoization[i] = result
+        return result
     }
 }
